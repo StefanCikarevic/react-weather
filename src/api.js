1c1142973085443b6630b848eb7c5a0242fe00ef
StefanCikarevic <stefancikarevic1996@gmail.com>
2022-08-25 15:26:22 +0200

    Finish!

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+        'X-RapidAPI-Key': '95a30dfademshfe3ee292bff8e19p14c33djsnf488f5e0c1b9',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "33bc73a7e363f58aa4d21f1e02ce1218"; 
